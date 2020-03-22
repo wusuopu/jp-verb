62f3055dedabf0101e0218f71281e05488817012
wusuopu <wusuopu@users.noreply.github.com>
2020-03-22 05:53:14 +0000

    update ci/cd config a5b116e5d66bf881bd8d274532488f71116bba96

---
+++ precache-manifest.6132419a792e625dc065bef9aa67a7af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2c4aa1628b40572130fc1c8bc5d6d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860c056456cb847b6c82",
+    "url": "/static/js/2.69793181.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/2.69793181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b65fe71f362e4bb11d9",
+    "url": "/static/js/3.25ab8d0c.chunk.js"
+  },
+  {
+    "revision": "5002026598ed5d61cd3d68aaaa6f8e02",
+    "url": "/static/js/3.25ab8d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bff851aa136b792346d",
+    "url": "/static/js/4.893389cd.chunk.js"
+  },
+  {
+    "revision": "b162cd2db624f2d3c891",
+    "url": "/static/js/5.34ec456e.chunk.js"
+  },
+  {
+    "revision": "4be80028551640a6e09d",
+    "url": "/static/js/6.23b83e79.chunk.js"
+  },
+  {
+    "revision": "85ae1d117f2a883234ad",
+    "url": "/static/js/main.92e5e471.chunk.js"
+  },
+  {
+    "revision": "c97270c99596733a212e",
+    "url": "/static/js/runtime-main.ddd2b330.js"
+  }
+]);
